a1d8230bea65d17119e5c3b1d8220f5d425f8115
Cristian <tiancarvelasquez@gmail.com>
2024-04-15 07:27:46 -0500

    Portafolio

--- portafolio-template/portafolio/views/about.py
+++
@@ -1,9 +0,0 @@
-import reflex as rx
-from portafolio.components.heading import heading
-
-
-def about(description: str) -> rx.Component:
-    return rx.vstack(
-        heading("Sobre mí"),
-        rx.text(description)
-    )
